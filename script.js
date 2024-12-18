9b69d6243c2af42cab62d9be8279334863b3529f
Sri <vulapusrinath@gmail.com>
2024-12-19 00:57:38 +0530

    added audio files

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let h2 = document.querySelector("h2");
 let boxbefore = document.querySelector(".box");
 let hs = document.getElementById("hs");
+let randColor;
+
+//Array of Audios...
+let audio1 = document.getElementById("myAudio1");
+let audio2 = document.getElementById("myAudio2");
+let audio3 = document.getElementById("myAudio3");
+let audio4 = document.getElementById("myAudio4");
 
 let LifetimehighScore = 0;
 
@@ -23,6 +30,20 @@ document.addEventListener("keypress", function () {
 
 function btnFlsh(button) {
   button.classList.add("flash");
+  if (randColor == "btn1") {
+    audio1.play();
+    audio1.playbackRate = 2;
+  } else if (randColor == "btn2") {
+    audio2.play();
+    audio2.playbackRate = 1.25;
+  } else if (randColor == "btn3") {
+    audio3.play();
+    audio3.playbackRate = 2;
+  } else {
+    audio4.play();
+    audio4.playbackRate = 1.5;
+  }
+
   setTimeout(function () {
     button.classList.remove("flash");
   }, 300);
@@ -41,7 +62,7 @@ function lvlUp() {
   h2.innerText = `Level ${lvl}`;
 
   let randidx = Math.floor(Math.random() * 4);
-  let randColor = btns[randidx];
+  randColor = btns[randidx];
   let randbtn = document.querySelector(`.${randColor}`);
 
   ans.push(randColor);
@@ -54,6 +75,8 @@ function isValid(idx) {
       setTimeout(lvlUp, 1000);
     }
   } else {
+    let audio = document.getElementById("myAudio");
+    audio.play();
     h2.innerHTML = `Game Over! <u><i>Your Score : <b> ${lvl}</b></i></u>. Press any key to start`;
     if (lvl > LifetimehighScore) {
       LifetimehighScore = lvl;
